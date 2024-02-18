8d7600536498231b15c349f2bcf0e03372736062
Zain Ishtiaq <zainishtiaq.7866@gmail.com>
2024-02-18 02:46:50 -0800

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:attendenceapp/Signup_Signin_Screen/splash.dart';
 import 'package:attendenceapp/utils/color_utils.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
