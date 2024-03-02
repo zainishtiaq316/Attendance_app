03728e573861c14b1b0f58a5a736fa96b94c90dd
Zain Ishtiaq <zainishtiaq.7866@gmail.com>
2024-03-02 07:16:15 -0800

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:attendenceapp/Signup_Signin_Screen/splash.dart';
 import 'package:attendenceapp/utils/color_utils.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Attendence App',
+      title: 'Attendence App ',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: kPColor,
